2b38eec8887a07a09b05d9d5d50cadd57f47b138
임성민 <scouts33@naver.com>
2020-10-02 15:44:15 +0900

    add to list Test

--- eatgo-api/src/main/java/co/kr/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/co/kr/fastcampus/eatgo/domain/Restaurant.java
@@ -1,2 +1,30 @@
-package co.kr.fastcampus.eatgo.domain;public class Restaurant {
+package co.kr.fastcampus.eatgo.domain;
+
+public class Restaurant {
+
+    private final Long id;
+    private final String name;
+    private  final String address;
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
 }
--- eatgo-api/src/test/java/co/kr/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/co/kr/fastcampus/eatgo/domain/RestaurantTests.java
@@ -1,4 +1,24 @@
+package co.kr.fastcampus.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+
 public class RestaurantTests {
-  
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+        assertThat(restaurant.getId(), is(1004L));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
 }
